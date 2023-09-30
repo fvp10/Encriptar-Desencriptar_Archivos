d021b2464e0ba09ca6fac5a3ba96a435cda04354
fvp10 <el18delacross@gmail.com>
2023-09-30 11:49:42 +0200

    MENSAJE PRUEBA

--- Practica_CS_encriptar_desencriptar_F1/Form2.cs
+++ Practica_CS_encriptar_desencriptar_F1/Form2.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace Practica_CS_encriptar_desencriptar_F1
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void desencriptar__Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void encriptar__Click(object sender, EventArgs e)
-        {
-            //Valentino es subnormal
         }
     }
 }
