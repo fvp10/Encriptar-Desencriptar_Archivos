f88bd8ca8feaed1746b8e3d14a0799b1c3d13f79
FdeFlax <joseangel03sg@gmail.com>
2023-10-10 20:00:47 +0200

    cambios en ruta a rutaGuardado = "C:/CSarchivosENC"

--- Practica_CS_encriptar_desencriptar_F1/Form1.cs
+++ Practica_CS_encriptar_desencriptar_F1/Form1.cs
@@ -1,14 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Security.Cryptography;
 
 namespace Practica_CS_encriptar_desencriptar_F1
 {
@@ -17,7 +11,7 @@ namespace Practica_CS_encriptar_desencriptar_F1
     {
         private int posicionVertical = 10; // Inicializa la posición vertical
         private Dictionary<System.Windows.Forms.Button, Panel> botonesYFilas = new Dictionary<System.Windows.Forms.Button, Panel>();
-
+        private string rutaGuardado = "C:/CSarchivosENC";
         public Form1()
         {
             InitializeComponent();
@@ -30,8 +24,110 @@ namespace Practica_CS_encriptar_desencriptar_F1
 
         private void encriptar__Click(object sender, EventArgs e)
         {
+            MetodoDeEncriptado();
+        }
+
+        private void MetodoDeEncriptado()
+        {
+            // Verificar si se ha seleccionado un archivo
+            if (string.IsNullOrEmpty(txt1.Text))
+            {
+                MessageBox.Show("Por favor, seleccione un archivo para encriptar.");
+                return;
+            }
+
+            // Obtener la ruta del archivo seleccionado
+            string archivoOriginal = openFileDialog1.FileName;
+
+            // Generar una clave aleatoria
+            byte[] clave = GenerarClaveAleatoria();
+
+            // Generar un IV aleatorio
+            byte[] iv = GenerarIVAleatorio();
+
+            string nombreArchivo = Path.GetFileName(archivoOriginal);
+
+            // Crear una instancia de AES con la clave y el IV generados
+            using (Aes objetoAes = Aes.Create())
+            {
+                objetoAes.Key = clave;
+                objetoAes.IV = iv;
+                objetoAes.Mode = CipherMode.CBC; // Modo CBC
+
+                //Escribe la clave en otro archivo a parte
+                string archivoClave = Path.Combine(rutaGuardado, nombreArchivo + "_clave.txt");
+                File.WriteAllBytes(archivoClave, clave);
+                //Escribe elIV en otro archivo a parte
+                string archivoIV = Path.Combine(rutaGuardado, nombreArchivo + "_IV.txt");
+                File.WriteAllBytes(archivoIV, iv);
+
+                // Crear un flujo de cifrado
+                //el obtejo de ICryptoTransform es la transformación de archivo desencriptado a encriptado y viceversa
+                //se inicializa con el resultado del método "CreateEncryptor()" que es la interfaz que define la funcionalidad básica para transformar datos(cifrar/descifrar).
+                using (ICryptoTransform cifrado = objetoAes.CreateEncryptor())
+                {
+                    // Ruta donde se guardará el archivo encriptado
+                    string archivoEncriptado = Path.Combine(rutaGuardado, nombreArchivo + ".enc");
+
+                    // Crear un flujo de escritura para el archivo encriptado
+                    //archivoenc representa el archivo encriptado en el que estamos escribiendo datos
+                    //FileMode.Create es un modo para abrir archivos, el cual crea un nuevo archivo si no existe o lo sobrescribe
+                    using (FileStream archivoenc = new FileStream(archivoEncriptado, FileMode.Create))
+                    {
+                        // Escribir el IV en el archivo encriptado (para uso posterior durante la desencriptación)
+                        //archivoenc representa el archivo encriptado en el que estamos escribiendo datos
+                        //el 0 indica desde donde comienza a leer los bites
+                        archivoenc.Write(iv, 0, iv.Length);
+
+                        // Crear un flujo de lectura para el archivo original con FileMode.Open
+                        //Este flujo de lectura se utilizará para leer los datos del archivo original
+                        //y luego cifrarlos antes de escribirlos en el archivo encriptado.
+                        using (FileStream fsIn = new FileStream(archivoOriginal, FileMode.Open))
+                        {
+                            //Crear un flujo de cifrado para cifrar los datos
+                            using (CryptoStream cs = new CryptoStream(archivoenc, cifrado, CryptoStreamMode.Write))
+                            {
+                                // Leer el archivo original y escribir los datos cifrados en el archivo encriptado
+                                int data;
+                                while ((data = fsIn.ReadByte()) != -1) // va byte a byte escribiendo el cifrado correspondiente, si llega al final devuelve -1 y para el bucle
+                                {
+                                    cs.WriteByte((byte)data);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Notificar al usuario que la encriptación se ha completado
+            MessageBox.Show("Archivo encriptado y guardado en: " + rutaGuardado);
+
+            // Crear una nueva fila en la interfaz gráfica
             CrearNuevaFila();
         }
+
+        // Método para generar una clave aleatoria de 32 bytes (256 bits)
+        private byte[] GenerarClaveAleatoria()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] clave = new byte[32]; // 32 bytes para AES-256
+                rng.GetBytes(clave);
+                return clave;
+            }
+        }
+
+        // Método para generar un IV aleatorio de 16 bytes (128 bits)
+        private byte[] GenerarIVAleatorio()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] iv = new byte[16]; // 16 bytes para AES
+                rng.GetBytes(iv);
+                return iv;
+            }
+        }
+
 
         private void btn_selc_Click(object sender, EventArgs e)
         {
@@ -40,8 +136,8 @@ namespace Practica_CS_encriptar_desencriptar_F1
             string texto = openFileDialog1.FileName;
 
             if (File.Exists(openFileDialog1.FileName))
-            {         
-                TextReader leer = new StreamReader(texto);        
+            {
+                TextReader leer = new StreamReader(texto);
                 leer.Close();
             }
             txt1.Text = Path.GetFileName(texto);
@@ -59,7 +155,7 @@ namespace Practica_CS_encriptar_desencriptar_F1
             txtCifrado.Text = txt1.Text; //ESTO ES TEMPORAL YA QUE HABRÁ QUE PONER EL ARCHIVO CIFRADO
             txtCifrado.Width = 150;
             txtCifrado.Height = 30;
-            
+
             System.Windows.Forms.Button btnDesencriptar = new System.Windows.Forms.Button();
             btnDesencriptar.Text = "Desencriptar";
             btnDesencriptar.Width = 80;
@@ -93,15 +189,16 @@ namespace Practica_CS_encriptar_desencriptar_F1
 
             // Asigna un manejador de eventos a los botones de la fila
             btnDesencriptar.Click += desencriptar__Click;
-            btnBorrar.Click += (sender ,e) => EliminarFila(nuevaFila);
+            btnBorrar.Click += (sender, e) => EliminarFila(nuevaFila, txtCifrado.Text);//he quitado + ".enc"
 
             // Agrega la nueva fila al panel de contenedor
             panelContenedor.Controls.Add(nuevaFila);
 
             txt1.Text = "";
         }
-        private void EliminarFila(Panel fila)
+        private void EliminarFila(Panel fila,string nombreArchivoEncriptado)
         {
+            BorrarArchivoEncriptado(nombreArchivoEncriptado);
             // Elimina la fila del panel de contenedor y del diccionario
             panelContenedor.Controls.Remove(fila);
             // Busca y elimina la entrada correspondiente en el diccionario
@@ -112,6 +209,21 @@ namespace Practica_CS_encriptar_desencriptar_F1
                     botonesYFilas.Remove(kvp.Key);
                     break;
                 }
+            }
+        }
+        private void BorrarArchivoEncriptado(string nombreArchivo)
+        {
+            
+            string rutaArchivoEncriptado = Path.Combine(rutaGuardado, nombreArchivo+".enc");
+            string rutaClave = Path.Combine(rutaGuardado, nombreArchivo+"_clave.txt");
+            string rutaIV = Path.Combine(rutaGuardado, nombreArchivo + "_IV.txt");
+            // Verifica si existen los archivos
+            if (File.Exists(rutaArchivoEncriptado) && File.Exists(rutaClave) && File.Exists(rutaIV))
+            {
+                // Elimina los archivos encriptados
+                File.Delete(rutaArchivoEncriptado);
+                File.Delete(rutaClave);
+                File.Delete(rutaIV);
             }
         }
         private void panelContenedor_Paint(object sender, PaintEventArgs e)
