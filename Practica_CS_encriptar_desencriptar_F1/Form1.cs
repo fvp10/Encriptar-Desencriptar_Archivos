8a6e99e107aa8e8633e8410c562b5fe2deca3d83
FdeFlax <joseangel03sg@gmail.com>
2023-10-11 18:04:32 +0200

    crea la carpeta para los archivos y la localiza

--- Practica_CS_encriptar_desencriptar_F1/Form1.cs
+++ Practica_CS_encriptar_desencriptar_F1/Form1.cs
@@ -16,6 +16,7 @@ namespace Practica_CS_encriptar_desencriptar_F1
         {
             InitializeComponent();
             ComprobarArchivosEncriptados();
+            ComprobarCarpeta();
         }
 
         private void desencriptar__Click(object sender, EventArgs e)
@@ -135,41 +136,6 @@ namespace Practica_CS_encriptar_desencriptar_F1
             }
         }
 
-        private void ComprobarArchivosEncriptados()
-        {
-            if (!Directory.Exists(rutaGuardado))
-            {
-                return;
-            }
-
-            // Buscar archivos en la carpeta de guardado
-            string[] archivos = Directory.GetFiles(rutaGuardado, "*.enc");
-            foreach (string archivo in archivos)
-            {
-                string nombreArchivo = Path.GetFileNameWithoutExtension(archivo);
-                string claveFile = Path.Combine(rutaGuardado, nombreArchivo + "_clave.txt");
-                string ivFile = Path.Combine(rutaGuardado, nombreArchivo + "_IV.txt");
-
-                // Verificar si existen los archivos de clave y IV
-                if (File.Exists(claveFile) && File.Exists(ivFile))
-                {
-                    // Crear una nueva fila en la interfaz gráfica
-                    CrearNuevaFila();
-
-                    // Agregar el nombre del archivo encriptado y la fecha de creación en la fila
-                    foreach (Panel fila in panelContenedor.Controls)
-                    {
-                        Label txtCifrado = (Label)fila.Controls[0];
-                        if (txtCifrado.Text == "")
-                        {
-                            txtCifrado.Text = nombreArchivo;
-                            txtCifrado.Width = 150;
-                            txtCifrado.Height = 30;
-                        }
-                    }
-                }
-            }
-        }
 
 
         private void btn_selc_Click(object sender, EventArgs e)
@@ -273,5 +239,55 @@ namespace Practica_CS_encriptar_desencriptar_F1
         {
 
         }
+
+        private void ComprobarArchivosEncriptados()
+        {
+            if (!Directory.Exists(rutaGuardado))
+            {
+                return;
+            }
+
+            // Buscar archivos en la carpeta de guardado
+            string[] archivos = Directory.GetFiles(rutaGuardado, "*.enc");
+            foreach (string archivo in archivos)
+            {
+                string nombreArchivo = Path.GetFileNameWithoutExtension(archivo);
+                string claveFile = Path.Combine(rutaGuardado, nombreArchivo + "_clave.txt");
+                string ivFile = Path.Combine(rutaGuardado, nombreArchivo + "_IV.txt");
+
+                // Verificar si existen los archivos de clave y IV
+                if (File.Exists(claveFile) && File.Exists(ivFile))
+                {
+                    // Crear una nueva fila en la interfaz gráfica
+                    CrearNuevaFila();
+
+                    // Agregar el nombre del archivo encriptado y la fecha de creación en la fila
+                    foreach (Panel fila in panelContenedor.Controls)
+                    {
+                        Label txtCifrado = (Label)fila.Controls[0];
+                        if (txtCifrado.Text == "")
+                        {
+                            txtCifrado.Text = nombreArchivo;
+                            txtCifrado.Width = 150;
+                            txtCifrado.Height = 30;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ComprobarCarpeta()
+        {
+            if (Directory.Exists("../Practica_CS_encriptar_desencriptar_F1/CSarchivos"))
+            {
+
+            }
+            else
+            {
+                
+            }
+        }
+
+
     }
 }
