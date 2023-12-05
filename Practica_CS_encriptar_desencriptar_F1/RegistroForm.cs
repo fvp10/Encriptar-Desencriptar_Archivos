c009dced351478effbe2d150fb90e981d3afec77
FdeFlax <joseangel03sg@gmail.com>
2023-12-05 23:20:46 +0100

    nuget

--- Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
+++ Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
@@ -1,14 +1,9 @@
 ﻿using Newtonsoft.Json;
+
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Practica_CS_encriptar_desencriptar_F1
