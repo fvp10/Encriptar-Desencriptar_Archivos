d76c984672758a853cf11b03c5e1028de6a067ce
YohnB <ybbb2@gcloud.ua.es>
2023-12-05 23:16:11 +0100

    Cambio

--- Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
+++ Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -32,11 +33,15 @@ namespace Practica_CS_encriptar_desencriptar_F1
             if (!Directory.Exists(Path.Combine(Application.StartupPath, "Usuarios", username)))
             {
                 Directory.CreateDirectory(Path.Combine(Application.StartupPath, "Usuarios", username));
-                string hashedPassword = GetSHA256Hash(password); // Debes definir este método en tu RegistroForm o mejor en una clase aparte
 
-                // Divide el hash para generar las claves y el hash de login
-                var (kDatos, kLogin) = SplitHash(hashedPassword); // Debes definir este método
-                kLogin = BCrypt.Net.BCrypt.HashPassword(kLogin);
+                string hashedPassword = GetSHA256Hash(password);
+
+                // Divide el hash en dos partes iguales
+                int halfLength = hashedPassword.Length / 2;
+                string kdatos = hashedPassword.Substring(0, halfLength);
+                string klogin = hashedPassword.Substring(halfLength);
+
+                kLogin = BCrypt.Net.BCrypt.HashPassword(klogin);
 
                 var userData = new
                 {
@@ -68,6 +73,30 @@ namespace Practica_CS_encriptar_desencriptar_F1
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GetSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Convierte la cadena en bytes y calcula el hash
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                // Convierte el hash en una cadena hexadecimal
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    stringBuilder.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return stringBuilder.ToString();
+            }
         }
     }
 }
