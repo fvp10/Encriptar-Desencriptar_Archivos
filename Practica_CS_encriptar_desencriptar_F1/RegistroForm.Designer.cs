d5c077577afe6980bbebdaedc1b9f3f9379f9817
FdeFlax <joseangel03sg@gmail.com>
2023-12-05 22:21:00 +0100

    1

--- Practica_CS_encriptar_desencriptar_F1/RegistroForm.Designer.cs
+++ Practica_CS_encriptar_desencriptar_F1/RegistroForm.Designer.cs
@@ -31,8 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtContrasenya = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
@@ -63,19 +63,20 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Contraseña";
             // 
-            // textBox1
+            // txtUsername
             // 
-            this.textBox1.Location = new System.Drawing.Point(360, 110);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 3;
+            this.txtUsername.Location = new System.Drawing.Point(360, 110);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.TabIndex = 3;
             // 
-            // textBox2
+            // txtContrasenya
             // 
-            this.textBox2.Location = new System.Drawing.Point(360, 171);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 4;
+            this.txtContrasenya.Location = new System.Drawing.Point(360, 171);
+            this.txtContrasenya.Name = "txtContrasenya";
+            this.txtContrasenya.Size = new System.Drawing.Size(100, 22);
+            this.txtContrasenya.TabIndex = 4;
+            this.txtContrasenya.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // linkLabel1
             // 
@@ -88,18 +89,18 @@
             this.linkLabel1.Text = "Iniciar Sesión";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // Registro
+            // RegistroForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.linkLabel1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtContrasenya);
+            this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Name = "Registro";
+            this.Name = "RegistroForm";
             this.Text = "Registro";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -111,8 +112,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtContrasenya;
         private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
+++ Practica_CS_encriptar_desencriptar_F1/RegistroForm.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,9 +20,54 @@ namespace Practica_CS_encriptar_desencriptar_F1
         }
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            this.Hide(); // OCULTAMOS EL LOGIN
-            LoginForm login = new LoginForm();   //CREAMOS UN NUEVO FORM 
-            login.Show(); //LO MOSTRAMOS 
+            registroMetodo();
+        }
+
+        public void registroMetodo()
+        {
+            string username = txtUsername.Text;
+            string password = txtContrasenya.Text;
+
+            // Asumiendo que tienes métodos similares a los de UsuarioModel.cs en tu cliente
+            if (!Directory.Exists(Path.Combine(Application.StartupPath, "Usuarios", username)))
+            {
+                Directory.CreateDirectory(Path.Combine(Application.StartupPath, "Usuarios", username));
+                string hashedPassword = GetSHA256Hash(password); // Debes definir este método en tu RegistroForm o mejor en una clase aparte
+
+                // Divide el hash para generar las claves y el hash de login
+                var (kDatos, kLogin) = SplitHash(hashedPassword); // Debes definir este método
+                kLogin = BCrypt.Net.BCrypt.HashPassword(kLogin);
+
+                var userData = new
+                {
+                    NombreUsuario = username,
+                    KLogin = kLogin
+                };
+
+                // Guarda el usuario.json en el directorio del usuario
+                File.WriteAllText(Path.Combine(Application.StartupPath, "Usuarios", username, "usuario.json"), JsonSerializer.Serialize(userData));
+
+                // Genera las claves criptográficas RSA
+                var (publicKey, privateKey) = GenerateRSAKeys(); // Debes definir este método
+                var encryptedPrivateKey = EncryptWithPassword(privateKey, kDatos); // Debes definir este método
+
+                // Guarda las claves en archivos
+                File.WriteAllText(Path.Combine(Application.StartupPath, "Usuarios", username, "publicKey.xml"), publicKey);
+                File.WriteAllText(Path.Combine(Application.StartupPath, "Usuarios", username, "privateKeyEncrypted.xml"), encryptedPrivateKey);
+
+                MessageBox.Show("Registro exitoso.");
+                // Aquí puedes cerrar el formulario de registro o redirigir al usuario
+            }
+            else
+            {
+                MessageBox.Show("El nombre de usuario ya existe.");
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
